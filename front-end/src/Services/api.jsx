c217a3b34f5ad0ffa7bcb9ef8d75b09f9282267a
Mohd Salman <152871570+salman123441512@users.noreply.github.com>
2024-09-16 12:05:51 +0530

    Delete front-end directory

--- front-end/src/Services/api.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api';
-
-export const createUser = (userData) => axios.post(`${API_URL}/users`, userData);
-
-export const getUserByAccountNumber = (accountNumber) => axios.get(`${API_URL}/users/${accountNumber}`);
-
-export const updateAmount = (accountNumber, data) => axios.put(`${API_URL}/users/${accountNumber}`, data);
